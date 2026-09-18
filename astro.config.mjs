1de2d7e1b1d1f8ba6c4dd8594f165df4287fa3fb
morekaoficial-lgtm <morekaoficial@gmail.com>
2026-09-18 14:03:16 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://github.com/wistermarquez90-oss',
+  site: 'https://github.com/morekaoficial-lgtm',
   base: '/landing-moreka',
   integrations: [tailwind()]
 });
